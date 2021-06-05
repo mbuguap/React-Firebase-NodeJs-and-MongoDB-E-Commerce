8214659b61f4031df81e3682b26d4eb8db1e64b7
mbuguap <petrmbugua@gmail.com>
2021-06-05 16:59:06 +0300

    installations

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+
+
+require("dotenv").config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log("DB CONNECTION ERR", err));
+
+// middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+// routes middleware
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
+
+// port
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
